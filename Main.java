3821b75710a47d7619054158682e4f856fab56dd
KarraRohithKumar <karrarohithkumar@gmail.com>
2024-10-21 15:38:56 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,15 @@
-package Encapselation;
-import java.util.Scanner;
+package Super_constructor_keys;
 
+/**
+ * Main
+ */
 public class Main {
-    public static void main(String[] args) {
-        // Create a bank account
-        MiniProject myAccount = new MiniProject("Rohith Kumar", "123456789", 1000000.0);
 
-        Scanner sc = new Scanner(System.in);
-        boolean exit = false;
+//     public static void main(String[] args) {
+//         Area_of_circle circle=new Area_of_circle();
+// System.out.println(circle.area_of_circle());
+// Area_of_triangle obj=new Area_of_triangle();
+// System.out.println(obj.area_of_rombus(5,2));
+//     }
 
-        // Bank account operations menu
-        while (!exit) {
-            System.out.println("\nBank Account Menu");
-            System.out.println("1. Deposit Money");
-            System.out.println("2. Withdraw Money");
-            System.out.println("3. Check Balance");
-            System.out.println("4. Exit");
-            System.out.print("Choose an option: ");
-            int choice = sc.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter deposit amount: ");
-                    double depositAmount = sc.nextDouble();
-                    myAccount.deposit(depositAmount);
-                    break;
-
-                case 2:
-                    System.out.print("Enter withdrawal amount: ");
-                    double withdrawAmount = sc.nextDouble();
-                    myAccount.withdraw(withdrawAmount);
-                    break;
-
-                case 3:
-                    System.out.println("Current Balance: $" + myAccount.getBalance());
-                    break;
-
-                case 4:
-                    System.out.println("Exiting...");
-                    exit = true;
-                    break;
-
-                default:
-                    System.out.println("Invalid option. Please try again.");
-            }
-        }
-
-        sc.close();
-    }
 }
