249e5699679b6740448237c272ec106dd5c29882
KarraRohithKumar <karrarohithkumar@gmail.com>
2024-10-25 22:15:56 +0530

    Add files via upload

--- Basic2.java
+++ Basic2.java
@@ -1,28 +1,24 @@
 package Bubblesort;
 
-public class Basic {
-   
-        public static void bubbleSort(int[] arr) {
+public class Basic2 {
+    
+        public static void bubbleSortDescending(int[] arr) {
             int n = arr.length;
-            boolean swapped;
             for (int i = 0; i < n - 1; i++) {
-                swapped = false;
                 for (int j = 0; j < n - i - 1; j++) {
-                    if (arr[j] > arr[j + 1]) {
+                    if (arr[j] < arr[j + 1]) {
                         int temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
-                        swapped = true;
                     }
                 }
-                if (!swapped) break;  // No swap means array is already sorted
             }
         }
     
         public static void main(String[] args) {
-            int[] arr = {3, 1, 4, 2, 5, 6};
-            bubbleSort(arr);
-            System.out.println("Sorted array: ");
+            int[] arr = {5, 1, 4, 2, 8};
+            bubbleSortDescending(arr);
+            System.out.println("Sorted array in descending order: ");
             for (int num : arr) {
                 System.out.print(num + " ");
             }
