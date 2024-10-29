1936e5451919435a10596b9cc5aaf2f382b720c0
KarraRohithKumar <karrarohithkumar@gmail.com>
2024-10-29 22:34:36 +0530

    Add files via upload

---
+++ Functional_Interface.java
@@ -0,0 +1,15 @@
+package JAVA1;
+@FunctionalInterface
+interface Greeting {
+    void sayHello(String name);
+}
+
+public class Functional_Interface {
+    public static void main(String[] args) {
+        // Using lambda expression to implement the functional interface
+        Greeting greeting = name -> System.out.println("Hello, " + name + "!");
+        
+        // Calling the method
+        greeting.sayHello("Rohith");
+    }
+}
