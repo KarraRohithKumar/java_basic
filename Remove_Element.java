e4e062650a0808b534e2c714bbe5ef3876f1c380
KarraRohithKumar <karrarohithkumar@gmail.com>
2024-11-18 21:45:05 +0530

    Add files via upload

---
+++ Remove_Element.java
@@ -0,0 +1,18 @@
+package Arrays;
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        for (int i = 0; i <= nums.length; i++) {
+            if(nums[i]==1){
+                int index=nums[i];
+                char n='_';
+            }
+        }
+    }
+}
+public class Remove_Element {
+    public static void main(String[] args) {
+        int[] nums={1,2,2,3};
+        int val=1;
+        int[] n=new int[nums.length];
+    }
+}
