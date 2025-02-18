7ac1202b98678b354c330503ef8e1780e945e908
KarraRohithKumar <karrarohithkumar@gmail.com>
2025-02-18 20:45:03 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,29 +1,24 @@
-import java.math.BigInteger;
-import java.util.Scanner;
+package Recursion;
 
 public class Factorial {
-    public static BigInteger factorial(int n) {
-        BigInteger result = BigInteger.ONE;
-        
-        for (int i = 2; i <= n; i++) {
-            result = result.multiply(BigInteger.valueOf(i));
+    public static void main(String[] arg) {
+       int n=6;
+       System.out.println(factorial(n));
+    }
+    static int factorial(int n){
+        //Base case
+        if(n==1 || n==0){
+            return 1;
         }
-        
+        //Recursive case
+         int result=n*factorial(n-1);
         return result;
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        
-        if (number < 0) {
-            System.out.println("Factorial is not defined for negative numbers.");
-        } else {
-            System.out.println("Factorial of " + number + " is: " + factorial(number));
-        }
-        
-        scanner.close();
-    }
 }
+   // int n = 3;
+        // int fact = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     fact = fact * i;
+
+        // }
+        // System.out.println(fact);
